aef316d62b5efa5796a7c16c168da4e0a5a01231
Infinity <Kinglseyoforiatta70@gmail.com>
2024-12-18 14:43:46 +0000

    Kingsley10

--- Advanced_Vehicle_Rental_Management_System/src/Main.java
+++ Advanced_Vehicle_Rental_Management_System/src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
+        Car car = new Car("E53","Mercedes-AMG",500.00,true,true);
+        Motorcycle motorcycle = new Motorcycle("","",200.0,true,false);
+        Truck truck =  new Truck("","",400.00,true,1500);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            agency.addVehicle(car);
+            agency.addVehicle(motorcycle);
+            agency.addVehicle(truck);
+
+            Customer customer = new Customer("Kingsley Ofori-Atta");
+            try{
+                agency.rentVehicle("E53",customer,5);
+                agency.rentVehicle("",customer,3);
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            agency.returnVehicle(car,customer);
     }
 }
